84f276b2e715079f58817dc8f841a12f24adcac3
SRISHA <lohagaonkarsrisha@gmail.com>
2026-07-14 19:52:51 +0530

    practised more better logic codes today

---
+++ program16.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+    int iRemainder = 0;
+
+    if (iNo % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("enter no to check wether number is even or odd : ");
+    printf("Enter number : \n");
+    scanf("%d,&iValue");
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is even\n",iValue);
+    }
+    else
+    {
+        printf("%d is odd\n",iValue);
+    }
+
+    return 0;
+}
