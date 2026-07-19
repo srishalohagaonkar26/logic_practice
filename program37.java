6690ade324cd53e52b3b87c4a0dadecdbdc4177b
SRISHA <lohagaonkarsrisha@gmail.com>
2026-07-20 00:13:12 +0530

    adding files

---
+++ program37.java
@@ -0,0 +1,12 @@
+class program37
+{
+    public static void main(String[] A) 
+    {
+        int iCnt = 0;       //loop counter
+
+        for(iCnt = 1; iCnt <= 10; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
