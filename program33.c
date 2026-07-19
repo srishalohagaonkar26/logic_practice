a4662c762b858f298589eeb2e0f2cb530466e8c2
SRISHA <lohagaonkarsrisha@gmail.com>
2026-07-19 20:09:15 +0530

    adding files

---
+++ program33.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    //filter
+    if(iNo < 0)
+    {
+        printf("Invalid input\n");
+        return;
+    }
+    for(iCnt = 0; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\n",iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the freqyency :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+}
