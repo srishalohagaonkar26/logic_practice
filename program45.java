549c9b5b5343af3fad9173e31c9d5cdc4649bda5
SRISHA <lohagaonkarsrisha@gmail.com>
2026-07-23 00:08:18 +0530

    adding files

---
+++ program45.java
@@ -0,0 +1,48 @@
+//type 3    
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean CheckDivisible(int iNo)       //static mule directly call
+    {
+        if((iNo %3 == 0) && (iNo % 5 ==0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+class program45
+{
+    public static void main(String[] A) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        boolean bRet = false;
+
+        System.out.println("Enter Number : ");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+        bRet = nobj.CheckDivisible(iValue);
+        
+        if(bRet == true)
+        {
+            System.out.println("Number is divisible by 3 and 5");
+        }
+        else
+        {
+            System.out.println("Numver is not divisible by 3 and 5");
+        }
+    }
+}
+
+
+
+
+
