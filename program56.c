27ff0513c3bd6744b24905dec1e3f6cef16b11cf
SRISHA <lohagaonkarsrisha@gmail.com>
2026-07-29 20:03:34 +0530

    adding files

---
+++ program56.c
@@ -0,0 +1,27 @@
+//input : 5
+//output : 1 2 3 4 
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt < iNo ; iCnt += 3)
+    {
+        printf("%d\t",iCnt);
+    }
+
+    printf("\n");
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);        //?????
+    return 0;
+}
