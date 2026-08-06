7123764c5a368dd676aa23cc4efbeda1310913e3
SRISHA <lohagaonkarsrisha@gmail.com>
2026-08-06 19:05:37 +0530

    adding files

---
+++ program71.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while( iCnt <= 5)
+    {
+        printf("JAY GANESH...\n");
+        iCnt++;
+    }
+    return 0;
+}
---
+++ program72.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    do
+    {
+        printf("JAY GANESH...\n");
+        iCnt++;
+    }while( iCnt <= 5);
+    return 0;
+}
