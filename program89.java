14e53cfe627c58aab55943f51dd6a9cc1ce7a25c
SRISHA <lohagaonkarsrisha@gmail.com>
2026-08-19 11:24:53 +0530

    adding files

---
+++ program89.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class DigitX
+{
+    public int SumDigits(int iNo)         //now we want functiom return value so int
+    {
+        int iDigit = 0;
+        int iSum = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;      //we get single digit
+            iSum = iSum + iDigit;
+        }
+        return iSum;
+    }
+}
+class program89
+{
+    public static void main(String[] args)
+{
+    Scanner sobj = new Scanner(System.in);
+    DigitX dobj = new DigitX();
+    int iValue = 0;        // declare FIRST
+    int iRet = 0;
+
+    System.out.println("Enter no : ");   // prompt
+    iValue = sobj.nextInt();             // read input
+    iRet = dobj.SumDigits(iValue);       // call function
+    System.out.println("Summation of digits :" + iRet);
+}
+}
